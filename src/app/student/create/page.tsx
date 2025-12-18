7c5254e0ef8996de24784d0837f009568c147c93
Shankar Dutta <shankardutta1719@gmail.com>
2025-12-18 16:22:33 +0530

    Student form design  done !
    due functionality & connect with database

--- src/app/student/create/page.tsx
+++ src/app/student/create/page.tsx
@@ -1,5 +1,17 @@
-const page = () => {
-	return <div>Enter</div>;
+import StudentForm from "@/components/Forms/StudentForm";
+import { Card } from "@/components/shadcnui/card";
+import prisma from "@/lib/database/dbClient";
+
+const page = async () => {
+	const TeacherData = await prisma.teacher.findMany();
+	return (
+		<div className="grid h-[95dvh] place-items-center">
+			<Card className="w-[300] gap-0 space-y-3 px-4">
+				<h1 className="text-center text-2xl font-bold">Create Student</h1>
+				<StudentForm teacherInfo={TeacherData} />
+			</Card>
+		</div>
+	);
 };
 
 export default page;
---
+++ src/components/Forms/StudentForm.tsx
@@ -0,0 +1,189 @@
+"use client";
+
+import { studentSchema, StudentSchemaType } from "@/lib/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { InfoIcon, LoaderIcon } from "lucide-react";
+import { Controller, useForm } from "react-hook-form";
+import { Teacher } from "../../../generated/prisma/browser";
+import { Button } from "../shadcnui/button";
+import { Field, FieldError, FieldLabel } from "../shadcnui/field";
+import { Input } from "../shadcnui/input";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "../shadcnui/select";
+
+type StudentFormPropsType = {
+	teacherInfo: Teacher[];
+};
+
+const StudentForm = ({ teacherInfo }: StudentFormPropsType) => {
+	const {
+		handleSubmit,
+		control,
+		formState: { isSubmitting },
+	} = useForm({
+		resolver: zodResolver(studentSchema),
+		defaultValues: {
+			firstName: "",
+			lastName: "",
+			emailId: "",
+			gender: "",
+			teacherId: "",
+		},
+
+		mode: "onChange",
+	});
+
+	const submitData = (fData: StudentSchemaType) => {
+		console.log(fData);
+	};
+	return (
+		<form
+			onSubmit={handleSubmit(submitData)}
+			noValidate
+			className="space-y-3">
+			<div className="flex items-center gap-3">
+				<Controller
+					name="firstName"
+					control={control}
+					render={({ field, fieldState }) => (
+						<Field data-invalid={fieldState.invalid}>
+							<FieldLabel htmlFor={field.name}>First Name</FieldLabel>
+							<Input
+								{...field}
+								id={field.name}
+								aria-invalid={fieldState.invalid}
+								placeholder="first name"
+								autoComplete="given-name"
+							/>
+
+							{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+						</Field>
+					)}
+				/>
+				<Controller
+					name="lastName"
+					control={control}
+					render={({ field, fieldState }) => (
+						<Field data-invalid={fieldState.invalid}>
+							<FieldLabel htmlFor={field.name}>Last Name </FieldLabel>
+							<Input
+								{...field}
+								id={field.name}
+								aria-invalid={fieldState.invalid}
+								placeholder="last name"
+								autoComplete="given-name"
+							/>
+
+							{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+						</Field>
+					)}
+				/>
+			</div>
+
+			<Controller
+				name="emailId"
+				control={control}
+				render={({ field, fieldState }) => (
+					<Field data-invalid={fieldState.invalid}>
+						<FieldLabel htmlFor={field.name}>Email Id </FieldLabel>
+						<Input
+							{...field}
+							id={field.name}
+							aria-invalid={fieldState.invalid}
+							placeholder="Enter your email id"
+							autoComplete="given-name"
+						/>
+
+						{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+					</Field>
+				)}
+			/>
+
+			<Controller
+				name="gender"
+				control={control}
+				render={({ field, fieldState }) => (
+					<Field data-invalid={fieldState.invalid}>
+						<FieldLabel htmlFor={field.name}>Genader </FieldLabel>
+
+						<Select
+							name={field.name}
+							value={field.value}
+							onValueChange={field.onChange}>
+							<SelectTrigger
+								id={field.name}
+								aria-invalid={fieldState.invalid}
+								className="min-w-[120px]">
+								<SelectValue placeholder="Select your gender" />
+							</SelectTrigger>
+
+							<SelectContent>
+								<SelectItem value="male">Male</SelectItem>
+								<SelectItem value="female">Female</SelectItem>
+								<SelectItem value="other">Other</SelectItem>
+							</SelectContent>
+						</Select>
+
+						{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+					</Field>
+				)}
+			/>
+
+			<Controller
+				name="teacherId"
+				control={control}
+				render={({ field, fieldState }) => (
+					<Field data-invalid={fieldState.invalid}>
+						<FieldLabel htmlFor={field.name}>Teacher </FieldLabel>
+
+						<Select
+							name={field.name}
+							value={field.value}
+							onValueChange={field.onChange}>
+							<SelectTrigger
+								id={field.name}
+								aria-invalid={fieldState.invalid}
+								className="min-w-[120px]">
+								<SelectValue placeholder="Select your teacher" />
+							</SelectTrigger>
+
+							<SelectContent>
+								{teacherInfo.map((teacher) => (
+									<SelectItem
+										value={`${teacher.firstName} ${teacher.lastName}`}
+										key={teacher.id}>
+										{teacher.firstName} {teacher.lastName}
+									</SelectItem>
+								))}
+							</SelectContent>
+						</Select>
+
+						{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+					</Field>
+				)}
+			/>
+
+			<Button
+				type="submit"
+				disabled={isSubmitting}
+				className="w-full cursor-pointer">
+				{isSubmitting ? (
+					<>
+						<LoaderIcon className="animate-spin" /> Submitting...
+					</>
+				) : (
+					<>
+						<InfoIcon /> Submit
+					</>
+				)}
+			</Button>
+		</form>
+	);
+};
+
+export default StudentForm;
